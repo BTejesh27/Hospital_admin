f9183c5e69dd8b2acae0d17352a877781e94a43c
harshavardhini39 <harshavardhinibudde@gmail.com>
2024-02-07 18:15:06 +0530

    tejesh

--- Profile Free Website Template - Free-CSS.com/profile/hospital_praj.sql
+++ Profile Free Website Template - Free-CSS.com/profile/hospital_praj.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2024 at 05:35 AM
+-- Generation Time: Feb 07, 2024 at 01:44 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -20,6 +20,63 @@ SET time_zone = "+00:00";
 --
 -- Database: `hospital_praj`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `123`
+--
+
+CREATE TABLE `123` (
+  `date` date NOT NULL,
+  `9-10` varchar(12) NOT NULL,
+  `10-11` varchar(12) NOT NULL,
+  `11-12` varchar(12) NOT NULL,
+  `12-1` varchar(12) NOT NULL,
+  `1-2` varchar(12) NOT NULL,
+  `2-3` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `123`
+--
+
+INSERT INTO `123` (`date`, `9-10`, `10-11`, `11-12`, `12-1`, `1-2`, `2-3`) VALUES
+('2024-02-01', '', '', 'yes', '', '', 'yes'),
+('2024-02-02', '', 'yes', 'yes', 'yes', 'yes', ''),
+('2024-02-09', '', 'yes', '', '', 'yes', ''),
+('2024-02-10', 'yes', '', 'yes', 'yes', 'yes', ''),
+('2024-02-14', '', '', '', 'yes', '', ''),
+('2024-02-24', 'yes', '', 'yes', 'yes', '', 'yes'),
+('2024-02-26', 'yes', '', '', '', '', ''),
+('2024-02-28', 'yes', '', '', '', 'yes', 'yes'),
+('2024-02-29', 'yes', '', '', '', '', 'yes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `9347`
+--
+
+CREATE TABLE `9347` (
+  `date` date NOT NULL,
+  `9-10` varchar(12) DEFAULT NULL,
+  `10-11` varchar(12) DEFAULT NULL,
+  `11-12` varchar(12) DEFAULT NULL,
+  `12-1` varchar(12) DEFAULT NULL,
+  `1-2` varchar(12) DEFAULT NULL,
+  `2-3` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `9347`
+--
+
+INSERT INTO `9347` (`date`, `9-10`, `10-11`, `11-12`, `12-1`, `1-2`, `2-3`) VALUES
+('2024-02-03', 'yes', 'yes', 'yes', NULL, NULL, NULL),
+('2024-02-08', 'yes', 'yes', 'yes', NULL, NULL, NULL),
+('2024-02-17', 'yes', NULL, 'yes', 'yes', NULL, NULL),
+('2024-02-23', NULL, 'yes', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -61,7 +118,7 @@ INSERT INTO `doctors` (`D_id`, `D_name`, `Experience`, `D_number`, `D_password`)
 (0, 'tejesh_b', 0, '', 321),
 (123, 'Avinash', 5, '9347923953', 123),
 (321, 'Somu', 6, '7892345656', 12345),
-(9876, '', 0, '', 1234),
+(9347, 'Tejesh_B', 0, '9347923953', 9347),
 (123456, '', 0, '', 123456);
 
 -- --------------------------------------------------------
@@ -106,6 +163,18 @@ INSERT INTO `slot` (`slot_id`, `D_id`, `Time`, `Date`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `123`
+--
+ALTER TABLE `123`
+  ADD PRIMARY KEY (`date`);
+
+--
+-- Indexes for table `9347`
+--
+ALTER TABLE `9347`
+  ADD PRIMARY KEY (`date`);
 
 --
 -- Indexes for table `appointment`
